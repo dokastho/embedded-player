1f9b9f967032b9bfc5bf067f472d76ceae99d296
Thomas (T.J.) Dokas <tjdokas@gmail.com>
2024-02-09 20:06:44 -0500

    Add ESP-environment files

--- main/main.c
+++ main/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-int
-main()
+void app_main(void)
 {
     printf("it works\n");
-    return 0;
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    fflush(stdout);
+    esp_restart();
 }
